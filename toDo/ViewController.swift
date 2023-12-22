be5c2b71569e17b240654eea07dc69a7529ccf82
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2023-12-23 00:31:00 +0900

    할일 삭제기능 추가

--- toDo/ViewController.swift
+++ toDo/ViewController.swift
@@ -11,18 +11,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var toDoListTableView: UITableView!
     @IBOutlet weak var addNewListButton: UIButton!
     
-    var toDoMemo: [Todo] = [
-           Todo(title: "할 일 1", isCompleted: false),
-           Todo(title: "할 일 2", isCompleted: false),
-           Todo(title: "할 일 3", isCompleted: false)
-       ]
-   
+    // toDoMemo 배열을 클래스 레벨로 이동
+    var toDoMemo: [Todo] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         toDoListTableView.backgroundColor = .systemYellow
         toDoListTableView.delegate = self
         toDoListTableView.dataSource = self
         addNewListButton.addTarget(self, action: #selector(addNewListButtonTapped), for: .touchUpInside)
+        
+        // 초기 데이터를 사용하여 toDoMemo 초기화
+        toDoMemo = [
+            Todo(title: "할 일 1", isCompleted: false),
+            Todo(title: "할 일 2", isCompleted: false),
+            Todo(title: "할 일 3", isCompleted: false)
+        ]
     }
     
     @IBAction func addNewListButtonTapped(_ sender: Any) {
@@ -30,35 +34,36 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func showAddTodoMessage() {
-        let alert = UIAlertController(title: "할 일 추가", message: nil, preferredStyle: .alert)
-        alert.addTextField { textField in
-            textField.placeholder = "할 일을 입력하세요"
-        }
-        
-        let addAction = UIAlertAction(title: "추가", style: .default) { [weak self] _ in
-            if let textField = alert.textFields?.first, let newTodoTitle = textField.text, !newTodoTitle.isEmpty {
-                let newTodo = Todo(title: newTodoTitle, isCompleted: false)
-                self?.addTodo(newTodo)
+            let alert = UIAlertController(title: "할 일 추가", message: nil, preferredStyle: .alert)
+            alert.addTextField { textField in
+                textField.placeholder = "할 일을 입력하세요"
             }
+            
+            let addAction = UIAlertAction(title: "추가", style: .default) { [weak self] _ in
+                if let textField = alert.textFields?.first, let newTodoTitle = textField.text, !newTodoTitle.isEmpty {
+                    let newTodo = Todo(title: newTodoTitle, isCompleted: false)
+                    self?.addTodo(newTodo)
+                }
+            }
+            
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            
+            alert.addAction(addAction)
+            alert.addAction(cancelAction)
+            
+            present(alert, animated: true, completion: nil)
         }
         
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        func addTodo(_ todo: Todo) {
+            toDoMemo.append(todo)
+            toDoListTableView.reloadData()
+        }
         
-        alert.addAction(addAction)
-        alert.addAction(cancelAction)
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return toDoMemo.count
+        }
         
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func addTodo(_ todo: Todo) {
-        toDoMemo.append(todo)
-        toDoListTableView.reloadData()
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return toDoMemo.count
-       }
-       
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
             let todo = toDoMemo[indexPath.row]
             
@@ -78,14 +83,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 attributedTitle.addAttribute(.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributedTitle.length))
             }
                 
-                cell.textLabel?.attributedText = attributedTitle
-                
-                return cell
+            cell.textLabel?.attributedText = attributedTitle
+            
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            // Todo의 완료 여부를 토글
+            toDoMemo[indexPath.row].isCompleted.toggle()
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        
+        // 테이블 뷰 셀 삭제 기능 추가
+        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                // Todo 삭제
+                toDoMemo.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
             }
-
-       func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-           // Todo의 완료 여부를 토글
-           toDoMemo[indexPath.row].isCompleted.toggle()
-           tableView.reloadRows(at: [indexPath], with: .automatic)
-       }
-   }
+        }
+    }
